55f97bf9be95c5775c007d0eaef2922d44285d77
Navneet Singh <navneetsinghh7905@gmail.com>
2023-12-30 23:34:22 +0530

    Portfolio

---
+++ Day11-Portfolio/main.js
@@ -0,0 +1,18 @@
+const dynamicContent = document.getElementById("dynamic-text");
+console.log(dynamicContent);
+
+let statement = "Web Developer...";
+
+let index = 0;
+
+const typeWriter = function(){
+    let new_title = statement.slice(0,index);
+    dynamicContent.innerText = new_title;
+    index>statement.length ? index=1: index++
+    // index++;
+    setTimeout(function(){
+        typeWriter();
+    }, 100);
+}
+
+typeWriter(statement);
---
+++ Day11-Portfolio/menu.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("menu_button").addEventListener("click", showMenu)
+    document.getElementById("closeMenu").addEventListener("click", closeMenu)
+    function showMenu(){
+        const menu= document.querySelector("section.menu")
+        menu.style.height="100%";
+        menu.style.width="100%";}
+
+    function closeMenu(){
+        const menu= document.querySelector("section.menu")
+        menu.style.height="0";
+        menu.style.width="0";}
+
+    function addClickToMenuItems(){
+        const menuItems = document.querySelectorAll("section.menu nav ul>li")
+        for(let menuItem of menuItems){
+            menuItem.addEventListener("click", closeMenu)
+        }
+    }
+
+    addClickToMenuItems();
+
+})
