065cd6b6c1e06d47ce17fb8b002b7d8bbd7c03a4
Navneet Singh <116378110+NavneetSinghh07@users.noreply.github.com>
2024-01-27 23:42:56 +0530

    Update main.js

--- Day18/main.js
+++ Day18/main.js
@@ -14,6 +14,7 @@ document.addEventListener("click", function(){
     console.log("THIS function gets call back when event occurs");
 })
 
+
 function callMe(fn){
     console.log("I'm done");
     fn();
